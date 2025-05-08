9ca2572b272718855108cda172cbd07780a1fbbd
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-05-08 08:59:19 -0300

    feat: estilizando o calendário

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -22,62 +22,100 @@ class _CalendarScreenState extends State<CalendarScreen> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-  }
-
-
-  @override
   Widget build(BuildContext context) {
+
+    const Color secondaryColor = const Color(0xFF568F80);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calendário',
+        title: const Text(
+          'Calendário',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TableCalendar(
-          locale: 'pt_BR',
-          firstDay: DateTime.utc(2010, 10, 20),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(_selectedDay, selectedDay)) {
-              setState(() {
-                _selectedDay = selectedDay;
+        padding: const EdgeInsets.all(12.0),
+        child: Card(
+          color: Colors.grey[200],
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TableCalendar(
+              availableCalendarFormats: const {CalendarFormat.month: 'month'},
+              calendarStyle: const CalendarStyle(
+                weekendTextStyle: TextStyle(
+                  color: Colors.red,
+                ),
+                todayDecoration: BoxDecoration(
+                  color: secondaryColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              locale: 'pt_BR',
+              firstDay: DateTime.utc(2010, 10, 20),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
                 _focusedDay = focusedDay;
-              });
-            }
-          },
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            _focusedDay = focusedDay;
-          },
-          headerStyle: const HeaderStyle(
-            formatButtonTextStyle:
-            TextStyle(color: Colors.white, fontSize: 16),
-            titleTextStyle: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20),
-            formatButtonDecoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              },
+              headerStyle: HeaderStyle(
+                titleCentered: true,
+                formatButtonTextStyle:
+                const TextStyle(color: Colors.white, fontSize: 16),
+                titleTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+                formatButtonDecoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                leftChevronIcon:
+                const Icon(Icons.chevron_left, color: Colors.green),
+                rightChevronIcon:
+                const Icon(Icons.chevron_right, color: Colors.green),
+              ),
+              calendarBuilders: CalendarBuilders(
+                headerTitleBuilder: (context, day) {
+                  final formattedDate =
+                  DateFormat.yMMMM('pt_BR').format(day);
+                  final capitalized = formattedDate[0].toUpperCase() +
+                      formattedDate.substring(1);
+                  return Center(
+                    child: Text(
+                      capitalized,
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-            leftChevronIcon:
-            Icon(Icons.chevron_left, color: Colors.blue),
-            rightChevronIcon:
-            Icon(Icons.chevron_right, color: Colors.blue),
           ),
         ),
       ),
