0d582e11cdd0824b4b4170b1380da159553175db
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-08-01 11:00:05 -0300

    feat: incluindo feriado e atualizando pubspec

--- lib/database/models/tipo_atividade_enum.dart
+++ lib/database/models/tipo_atividade_enum.dart
@@ -1,18 +1,18 @@
-/// Enum que representa o tipo de atividade para o registro de ponto.
 enum TipoAtividade {
   presencial,
   aula,
+  feriado
 }
 
-/// Extensão para adicionar métodos úteis ao enum TipoAtividade.
 extension TipoAtividadeExtension on TipoAtividade {
-  /// Retorna uma string legível para o tipo de atividade.
   String toDisplayString() {
     switch (this) {
       case TipoAtividade.presencial:
         return 'Presencial';
       case TipoAtividade.aula:
         return 'Aula';
+      case TipoAtividade.feriado:
+        return 'Feriado';
     }
   }
 }
